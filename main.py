d58537e06f60c1ef012a59470c75b70d5e74fbbd
simrs9 servercepat.cloud <41616116-simrs9@users.noreply.replit.com>
2025-04-17 04:35:45 +0000

    Restored to 'c8b2819cbfb6f13f44ea80c82cc44c036d62e42e'
    Replit-Restored-To: c8b2819cbfb6f13f44ea80c82cc44c036d62e42e

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import logging
-from app import app, db
+from app import app
 
 # Setup debugging
 logging.basicConfig(level=logging.DEBUG)
 
-def init_db():
-    with app.app_context():
-        db.create_all()
-
 if __name__ == "__main__":
-    init_db()  # Initialize database tables
     app.run(host="0.0.0.0", port=5000, debug=True)
